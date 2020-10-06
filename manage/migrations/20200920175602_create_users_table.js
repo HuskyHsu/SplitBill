7c29ab4d0bb638319c37d070d24366ccc61c3add
HuskyHsu <schi.hsu@gmail.com>
2020-10-06 23:58:34 +0800

    update migrations

--- manage/migrations/20200920175602_create_users_table.js
+++ manage/migrations/20200920175602_create_users_table.js
@@ -1,7 +1,8 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable('users', function(table) {
-        table.string('userId').primary();
+        table.increments('id');
+        table.string('userId');
         table.string('displayName').notNullable();
         table.string('pictureUrl').notNullable();
         table.string('statusMessage');
@@ -10,9 +11,12 @@ exports.up = function(knex) {
         
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
+
+        table.unique(['userId']);
       })
       .createTable('crowds', function(table) {
-        table.string('id').primary();
+        table.increments('id');
+        table.string('crowdId')
         table.enu('type', ['group', 'room']);
         table.string('groupName');
         table.string('pictureUrl');
@@ -20,12 +24,37 @@ exports.up = function(knex) {
         
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
+
+        table.unique(['crowdId']);
       })
-      .createTable('userJoin', function(table) {
+      .createTable('join_map', function(table) {
         table.string('userId').notNullable().references('users.userId');
-        table.string('crowdId').notNullable().references('crowds.id');
+        table.string('crowdId').notNullable().references('crowds.crowdId');
 
         table.unique(['userId', 'crowdId'])
+      })
+      .createTable('share_event', function(table) {
+        table.increments('id');
+        table.string('crowdId').notNullable().references('crowds.crowdId');
+        table.integer('price');
+        table.enu('splitType', ['AVG', 'EXTRA', 'PERCENT', 'WEIGHTS', 'SINGLE']);
+        table.string('status');
+        table.string('remark');
+
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+      })
+      .createTable('payer', function(table) {
+        table.increments('id');
+        table.string('userId').notNullable().references('users.userId');
+        table.integer('value');
+        table.integer('share_event_id').notNullable().references('share_event.id');
+      })
+      .createTable('debtor', function(table) {
+        table.increments('id');
+        table.string('userId').notNullable().references('users.userId');
+        table.integer('value');
+        table.integer('share_event_id').notNullable().references('share_event.id');
       })
 };
 
