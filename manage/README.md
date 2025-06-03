81644f93b63ee4604c6edf16c252faf42311a821
HuskyHsu <schi.hsu@gmail.com>
2025-06-03 23:43:01 +0800

    remove old file

--- manage/README.md
+++
@@ -1,2 +0,0 @@
-set KNEX_PATH=D:\Shih-Chi\Desktop\SplitBill\manage\www\node_modules\knex\knex.js
-knex migrate:latest
--- manage/www/models/db.js
+++
@@ -1,9 +0,0 @@
-
-const {DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_HOST} = process.env
-
-const knex = require('knex')({
-  client: 'pg',
-  connection: `postgresql://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`
-});
-
-module.exports = knex
